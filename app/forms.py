b6eb3c1b2deb1a45714ce4637e5853186946258e
Dmitry Strokov <dmitry.strokov@mail.ru>
2019-03-31 22:20:40 +0500

    Заполнение сущности информацией из формы

--- app/forms.py
+++ app/forms.py
@@ -10,6 +10,8 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+    ignored_fields = set(['submit', 'csrf_token', 'remember_me'])
+
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -17,6 +19,8 @@ class RegistrationForm(FlaskForm):
         validators=[DataRequired(), EqualTo('password')])
     recaptcha = RecaptchaField()
     submit = SubmitField('Register')
+
+    ignored_fields = set(['submit', 'csrf_token', 'password2', 'recaptcha'])
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -31,3 +35,5 @@ class UserProfileForm(FlaskForm):
     phone=StringField('Phone')
     info=StringField('Text')
     submit = SubmitField('Save')
+    
+    ignored_fields = set(['submit', 'csrf_token'])
