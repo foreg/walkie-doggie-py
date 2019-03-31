0d2101d6f595b006094690770af239eb458528ed
Dmitry Strokov <dmitry.strokov@mail.ru>
2019-03-31 22:50:47 +0500

    refactoring: using new features to fill user entity

--- app/controllers/controller_profile.py
+++ app/controllers/controller_profile.py
@@ -2,30 +2,19 @@ from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
 from app.forms import UserProfileForm
 from app.models import load_user
-from app.utils import login_required
+from app.utils import login_required, fill_entity
 
 
 @login_required
 def UserProfile(db):
     if current_user.is_authenticated:
-        form = UserProfileForm()
         user = current_user
+        form = UserProfileForm(obj=user)
         if form.validate_on_submit():
-            user.surname = form.surname.data   
-            user.name = form.name.data      
-            user.middlename = form.middlename.data
-            user.address = form.address.data    
-            user.phone = form.phone.data     
-            user.info = form.info.data      
+            errors, successfully = fill_entity(user, form)
+            print ("ENTITY {} FILLED WITH {} ERRORS, SUCCESSFULLY {}".format(user, errors, successfully))   
             db.session.add(user)
             db.session.commit()
             return render_template('profile.html', form=form)        
-        form.surname.data = user.surname
-        form.name.data = user.name
-        form.middlename.data = user.middlename
-        form.address.data = user.address
-        form.phone.data = user.phone
-        form.info.data = user.info
-        
         return render_template('profile.html', form=form)
     return redirect(url_for('login'))
