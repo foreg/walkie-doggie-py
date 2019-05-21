1643a4dc6a23d88aff647b3af1d2f99c8f7a8d39
Dmitry Strokov <dmitry.strokov@mail.ru>
2019-05-21 22:52:41 +0500

    ставка только на актуальный аукцион + кнопки завершения

--- app/static/js/history.js
+++ app/static/js/history.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('#dtBasicExample').DataTable();
+    $('#dtBasicExamplePast').DataTable();
     $('.dataTables_length').addClass('bs-select');
     $('.request').click((e)=>{
         let id = $(e.target).data('id');
@@ -7,5 +8,15 @@ $(document).ready(function () {
             id = $(e.target).parent().data('id')
         strHref = location.href.replace('history/', '');
         location.href = strHref + '/requests/' + id;
+    });
+    $('.requestPast').click((e)=>{
+        let id = $(e.target).data('id');
+        let pet_id = $(e.target).data('pet_id');
+        if (id == undefined)
+            id = $(e.target).parent().data('id')
+        if (pet_id == undefined)
+            pet_id = $(e.target).parent().data('pet_id')
+        strHref = "http://127.0.0.1:5000/"
+        location.href = strHref + 'pets/' + pet_id + '/requests/' + id;
     });
 });
